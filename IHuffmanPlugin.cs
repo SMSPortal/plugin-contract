55127af5984fc05eaf7a7e99f2249b62c09915ad
= <github@smsportal.co.za>
2015-11-02 14:45:15 +0200

    initial commit of contract

---
+++ IHuffmanPlugin.cs
@@ -0,0 +1,8 @@
+﻿namespace HuffmanPluginContract
+{
+    public interface IHuffmanPlugin
+    {
+        byte[] Compress(string data, out string tree);
+        string Decompress(byte[] data, string tree);
+    }
+}
